10fb818de023796bfda7316194e74ed47b4ba2de
Sansan Al Kahfi <75774492+sansanalkahfi@users.noreply.github.com>
2025-02-20 17:23:43 +0700

    tester

---
+++ laravel-9/tests/Feature/EnvironmentTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use function PHPUnit\Framework\assertEquals;
+
+class EnvironmentTest extends TestCase
+{
+    public function test_env()
+    {
+        $appName = env("ENV_TEST");
+        self::assertEquals("Sansan Al Kahfi", $appName);
+    }
+
+    public function test_envDefaultValue()
+    {
+        $defaultTest = env("DEFAULT_TEST", "yah gak ada");
+        self::assertEquals("yah gak ada", $defaultTest);
+    }
+}
