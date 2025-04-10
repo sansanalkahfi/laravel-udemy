9c3ed6199de1f4d0b2423750b6e998411819803a
Sansan Al Kahfi <75774492+sansanalkahfi@users.noreply.github.com>
2025-04-10 16:39:36 +0700

    testnya

--- laravel-9/tests/Feature/ServiceContainerTest.php
+++ laravel-9/tests/Feature/ServiceContainerTest.php
@@ -4,8 +4,10 @@ namespace Tests\Feature;
 
 use App\Data\Bar;
 use App\Data\Foo;
+use App\Data\Person;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Assert;
 use PhpParser\Builder\Function_;
 use Tests\TestCase;
 
@@ -24,6 +26,20 @@ class ServiceContainerTest extends TestCase
         self::assertNotSame($vFoo, $vfoo2);
     }
 
-    
+    public function testBind()
+    {
+        $this->app->bind(Person::class, function($app){
+            return new Person("text firstname", "text lastname");
+        });
+
+        
+        $vPerson1 = $this->app->make(Person::class);
+        $vPerson2 = $this->app->make(Person::class);
+
+        self::assertEquals("text firstname", $vPerson1->firstname);
+        self::assertEquals("text lastname", $vPerson2->lastname);
+        self::assertNotSame($vPerson1, $vPerson2);
+        
+    }
 
 }
