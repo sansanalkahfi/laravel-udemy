8278034cccc617158931f10c73ca98bb024a69c9
Sansan Al Kahfi <75774492+sansanalkahfi@users.noreply.github.com>
2025-05-26 21:24:25 +0700

    yoho

--- laravel-9/app/Http/Controllers/HelloController.php
+++ laravel-9/app/Http/Controllers/HelloController.php
@@ -10,12 +10,9 @@ class HelloController extends Controller
     private HelloServices $helloServices;
     public function __construct(HelloServices $helloServices)
     {
-        $this->helloServices = $helloServices;
+        $this->helloServices = $helloServices; // Inject HelloServices
     }
-    /* public function hello(): string
-    {
-        return 'Hello World';
-    } */
+
     public function hello(string $name): string
     {
         return $this->helloServices->hello($name);
