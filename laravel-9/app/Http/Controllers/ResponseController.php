732295755f9d38a614dc13b2cc57a9c222b3184b
Sansan Al Kahfi <75774492+sansanalkahfi@users.noreply.github.com>
2025-09-09 17:02:36 +0700

    ya

--- laravel-9/app/Http/Controllers/ResponseController.php
+++ laravel-9/app/Http/Controllers/ResponseController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ResponseController extends Controller
 {
-    //
+    public function response( Request $req): Response
+    {
+        return "Hello Response";
+    }
 }
